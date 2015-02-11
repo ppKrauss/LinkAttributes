23c8c5ac496ccceef7245322bbb11b42536b7d4b
Peter <ppkrauss@gmail.com>
2015-02-11 09:03:29 -0200

    add portuguese

--- LinkAttributes.i18n.php
+++ LinkAttributes.i18n.php
@@ -8,11 +8,12 @@
  
 $messages = array();
 
- 
 /** Swedish (Svenska)
  * @author Rotsee
  */
 $messages['sv'] = array(
         'linkattr-desc' => 'Gör det möjligt att sätta vissa html-attribut på länkar.',
 );
-
+$messages['pt'] = array(
+        'linkattr-desc' => 'Permite adicionar ou remover atributios HTML em links.',
+);
